c3e164eb084e58ba1b575ea19bf1bbfd43924089
Mohammed N <mnaser11218@gmail.com>
2025-01-30 10:41:18 -0500

    setup router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import Flexbox from './Components/Flexbox';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Notes from './Components/pages/Notes';
+import Create from './Components/pages/Create';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <Router>
+    <Routes>
+      <Route path='/notes' element={<Notes/>}/>
+      <Route path='/create' element={<Create/>}/>
+      <Route path='' element={<Create/>}/>
+    </Routes>
+   </Router>
+  // <Create/>
+  // <Notes/>
+    
   );
 }
 
